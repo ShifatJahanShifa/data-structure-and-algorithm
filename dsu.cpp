477a355f5f0a91627c442bea9ca6edcfc34e166a
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-03-14 00:13:02 +0600

    optimized

--- dsu.cpp
+++ dsu.cpp
@@ -4,10 +4,12 @@ using namespace std;
 const int N=1e5+10;
 
 int parent[N];
+int Size[N];
 
 void make(int v)
 {
     parent[v]=v;
+    Size[v]=1;
 }
 
 int Find(int v)
@@ -15,7 +17,7 @@ int Find(int v)
     if(v==parent[v])
         return v;
 
-    return Find(parent[v]);
+    return parent[v]=Find(parent[v]);
 }
 
 void Union(int a,int b)
@@ -25,7 +27,10 @@ void Union(int a,int b)
 
     if(a!=b)
     {
+        if(Size[a]<Size[b])
+            swap(a,b);
         parent[b]=a;
+        Size[a]+=Size[b];
     }
 }
 int main()
@@ -44,9 +49,13 @@ int main()
         Union(u,v);
     }
 
+    int cnt=0;
     for(int i=1;i<=n;i++)
     {
         cout<<parent[i]<<" ";
+        if(parent[i]==i)
+            cnt++;
     }
 
+    cout<<"\ncomponent "<<cnt;
 }
